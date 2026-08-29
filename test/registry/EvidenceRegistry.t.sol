628dc9bd525e4d445105b9e14478a86cdb5b81c7
etvjay <etvictory620@gmail.com>
2026-08-29 12:10:27 +0100

    test(m2): pin proof-native evidence identity

--- test/registry/EvidenceRegistry.t.sol
+++ test/registry/EvidenceRegistry.t.sol
@@ -12,20 +12,22 @@ contract EvidenceRegistryTest {
 
     function testEvidenceIsSingleRegistrationAndSingleConsumption() public {
         bytes32 domainId = keccak256("sepolia");
-        bytes32 txHash = keccak256("tx");
-        bytes32 evidenceId = registry.registerEvidence(domainId, 1, 100, txHash, 2, keccak256("payload"));
+        bytes32 encodedHash = keccak256("encoded");
+        bytes32 payloadHash = keccak256("payload");
+        bytes32 evidenceId = registry.registerEvidence(domainId, 1, 100, 7, 2, encodedHash, payloadHash);
 
         EvidenceRegistry.EvidenceRecord memory record = registry.getEvidence(evidenceId);
         require(record.gateway == address(this), "gateway");
+        require(record.txIndex == 7, "txIndex");
+        require(record.encodedTransactionHash == encodedHash, "encoded hash");
         require(!record.consumed, "premature consumption");
 
-        (bool duplicateOk,) = address(registry)
-            .call(
-                abi.encodeCall(
-                    registry.registerEvidence,
-                    (domainId, uint64(1), uint256(100), txHash, uint32(2), keccak256("payload"))
-                )
-            );
+        (bool duplicateOk,) = address(registry).call(
+            abi.encodeCall(
+                registry.registerEvidence,
+                (domainId, uint64(1), uint64(100), uint64(7), uint32(2), encodedHash, payloadHash)
+            )
+        );
         require(!duplicateOk, "duplicate evidence accepted");
 
         registry.consumeEvidence(evidenceId);
@@ -33,5 +35,12 @@ contract EvidenceRegistryTest {
 
         (bool replayOk,) = address(registry).call(abi.encodeCall(registry.consumeEvidence, (evidenceId)));
         require(!replayOk, "evidence consumed twice");
+    }
+
+    function testDifferentTransactionIndexCreatesDifferentEvidenceIdentity() public view {
+        bytes32 domainId = keccak256("sepolia");
+        bytes32 a = registry.computeEvidenceId(domainId, 1, 100, 7, 2);
+        bytes32 b = registry.computeEvidenceId(domainId, 1, 100, 8, 2);
+        require(a != b, "tx index must bind identity");
     }
 }
